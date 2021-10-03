3fcbd0156dc2a33a1977eb19d8b54d5dc5d5ab47
Hamza-Olol <91791454+Hamza-Olol@users.noreply.github.com>
2021-10-03 22:30:49 +0100

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+month = (input("Type in the first three letters of a month: ")).title()
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get(month))

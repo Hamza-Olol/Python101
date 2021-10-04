9419df0710047ec9c8e2e9e2e6145c67974050cd
Hamza-Olol <91791454+Hamza-Olol@users.noreply.github.com>
2021-10-04 20:38:04 +0100

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+#Madlibs game
+
+name = input("Pick a name: ")
+animal = input("Pick an animal: ")
+adj = input("Pick an adjective: ")
+
+madlib = f"{name} is a {animal}. \nIt is really {adj}."
+
+print(madlib)
